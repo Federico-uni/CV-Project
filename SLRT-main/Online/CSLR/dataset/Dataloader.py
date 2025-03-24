1891beae375c9bc1e98e0b6cc6cd727fdd3e6e64
Federico-uni <79905740+Federico-uni@users.noreply.github.com>
2025-03-24 18:04:11 +0100

    Update Dataloader.py

--- SLRT-main/Online/CSLR/dataset/Dataloader.py
+++ SLRT-main/Online/CSLR/dataset/Dataloader.py
@@ -100,6 +100,9 @@ def collate_fn_(batch, data_cfg, is_train, vocab, name2keypoint, word_emb_tab, v
             outputs['temp_idx'] = torch.tensor(outputs['temp_idx']).long()
 
         index_setting = data_cfg['transform_cfg'].get('index_setting', ['consecutive','pad','central','pad'])
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        print("data_cfg: ", data_cfg)
+         print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
         sgn_videos, sgn_keypoints, start_idx = load_batch_video(
             zip_file = None, #data_cfg['zip_file'], 
             names = outputs['names'], 
