71eea5a9f4bc2525f559872ad58fa29c0de4405d
Federico-uni <79905740+Federico-uni@users.noreply.github.com>
2025-03-25 16:24:48 +0100

    Update VideoLoader.py

--- SLRT-main/Online/CSLR/dataset/VideoLoader.py
+++ SLRT-main/Online/CSLR/dataset/VideoLoader.py
@@ -387,13 +387,10 @@ def load_batch_video(zip_file, names, vlens, raw_vlens, dataset_name, is_train,
             batch_videos.append(video) #wo transformed!!
 
             if name2keypoint != None:
-                if dataset_name in ['phoenix_iso', 'phoenix2014_iso', 'phoenix_comb_iso', 'csl_iso']:
+                if dataset_name in ['phoenix_iso', 'phoenix2014_iso', 'phoenix_comb_iso', 'csl_iso', 'IsolatedLIS']:
                     kps = name2keypoint[ori_vfile][selected_index,:,:]
                 else:
-                    json_train_path = "SLRT-main/TwoStreamNetwork/data/IsolatedLIS/annotations/train.json"
-                    video_id = find_video_id_by_url(json_train_path, name)
-                    video_id = video_id + ".mp4"
-                    kps = name2keypoint[video_id][selected_index,:,:]
+                    kps = name2keypoint[name][selected_index,:,:]
                 if pad is not None:
                     kps = pad_array(kps, pad)
 
